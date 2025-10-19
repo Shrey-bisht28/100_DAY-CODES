953eb91979170e6d8f2703654aba8a4da060bb10
Shrey-bisht28 <bishtshrey01@gmail.com>
2025-10-19 13:50:34 +0530

    Implement factor printing program in C
    Added a C program to print all factors of a given number.

---
+++ DAY_18(A)_factors.c
@@ -0,0 +1,21 @@
+//Write a program to print all factors of a given number.
+#include <stdio.h>
+
+int main() {
+    int num;
+
+    // Input the number
+    printf("Enter an integer: ");
+    scanf("%d", &num);
+
+    printf("Factors of %d are: ", num);
+
+    for (int i = 1; i <= num; i++) {
+        if (num % i == 0) {
+            printf("%d ", i);
+        }
+    }
+
+    printf("\n");
+    return 0;
+}
