ff36e0611eedcac49d191071678ed413d2a2bfee
Shrey-bisht28 <bishtshrey01@gmail.com>
2025-10-28 15:38:45 +0530

    Update DAY_27(A)_pattern.c

--- DAY_27(A)_pattern.c
+++ DAY_27(A)_pattern.c
@@ -14,7 +14,7 @@ int main() {
     int i, j;
     int n = 5; // number of rows in the top half (maximum stars)
 
-    // Top half of the pattern
+    
     for(i = 1; i <= n; i++) {
         for(j = 1; j <= 2*i - 1; j++) {
             printf("*");
@@ -22,7 +22,7 @@ int main() {
         printf("\n");
     }
 
-    // Bottom half of the pattern
+
     for(i = n-1; i >= 1; i--) {
         for(j = 1; j <= 2*i - 1; j++) {
             printf("*");
