857790a047779c9ecfaa216945ec2ebdfdaf2f85
Shrey-bisht28 <bishtshrey01@gmail.com>
2025-10-28 15:40:41 +0530

    Update DAY_25(A)_num pattern.c

--- DAY_25(A)_num pattern.c
+++ DAY_25(A)_num pattern.c
@@ -9,13 +9,13 @@
 int main() {
     int i, j, start;
 
-    // Outer loop for rows
+    
     for (i = 5; i >= 1; i--) {
-        // Print numbers from i to 5
+       
         for (j = i; j <= 5; j++) {
             printf("%d", j);
         }
-        printf("\n"); // Move to the next line after each row
+        printf("\n"); 
     }
 
     return 0;
