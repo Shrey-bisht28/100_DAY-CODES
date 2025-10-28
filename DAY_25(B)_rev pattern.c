728223e2d363445eed8b37a54d9d2110f2e24905
Shrey-bisht28 <bishtshrey01@gmail.com>
2025-10-28 15:40:25 +0530

    Refactor spacing and comments in pattern.c

--- DAY_25(B)_rev pattern.c
+++ DAY_25(B)_rev pattern.c
@@ -8,19 +8,19 @@
 
 int main() {
     int i, j, space;
-    int n = 5; // number of rows
+    int n = 5; 
 
-    // Outer loop for rows
+   
     for (i = 0; i < n; i++) {
         // Print leading spaces
         for (space = 0; space < i; space++) {
             printf(" ");
         }
-        // Print stars
+        
         for (j = i; j < n; j++) {
             printf("*");
         }
-        printf("\n"); // Move to next line after each row
+        printf("\n"); 
     }
 
     return 0;
