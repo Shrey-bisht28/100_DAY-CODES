1b48538c447878ee4b20f6d34fed86b4586e286d
Shrey-bisht28 <bishtshrey01@gmail.com>
2025-11-03 12:12:31 +0530

    Create DAY_47(A)_anagrams.c

---
+++ DAY_47(A)_anagrams.c
@@ -0,0 +1,46 @@
+//Check if two strings are anagrams of each other.
+#include <stdio.h>
+
+int main() {
+    char str1[100], str2[100];
+    int freq1[26] = {0}, freq2[26] = {0};
+    int i = 0;
+
+    printf("Enter first string: ");
+    fgets(str1, sizeof(str1), stdin);
+
+    printf("Enter second string: ");
+    fgets(str2, sizeof(str2), stdin);
+
+   
+    while (str1[i] != '\0') {
+        if (str1[i] >= 'a' && str1[i] <= 'z') {
+            freq1[str1[i] - 'a']++;
+        } else if (str1[i] >= 'A' && str1[i] <= 'Z') {  
+            freq1[str1[i] - 'A']++;
+        }
+        i++;
+    }
+
+    i = 0;
+   
+    while (str2[i] != '\0') {
+        if (str2[i] >= 'a' && str2[i] <= 'z') {
+            freq2[str2[i] - 'a']++;
+        } else if (str2[i] >= 'A' && str2[i] <= 'Z') {  
+            freq2[str2[i] - 'A']++;
+        }
+        i++;
+    }
+
+    for (i = 0; i < 26; i++) {
+        if (freq1[i] != freq2[i]) {
+            printf("The strings are not anagrams.\n");
+            return 0;
+        }
+    }
+
+    printf("The strings are anagrams.\n");
+    return 0;
+}
+
