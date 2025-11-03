6285a9460990d0d5f923d495453c393d26eb9eaf
Shrey-bisht28 <bishtshrey01@gmail.com>
2025-11-03 12:03:30 +0530

    Create DAY_43(B)_palindrome.c

---
+++ DAY_43(B)_palindrome.c
@@ -0,0 +1,36 @@
+//Check if a string is a palindrome.
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int length = 0, i, isPalindrome = 1;
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);  
+
+   
+    while (str[length] != '\0') {
+        length++;
+    }
+
+    
+    if (length > 0 && str[length - 1] == '\n') {
+        str[length - 1] = '\0';
+        length--;
+    }
+
+    
+    for (i = 0; i < length / 2; i++) {
+        if (str[i] != str[length - i - 1]) {
+            isPalindrome = 0;
+            break;
+        }
+    }
+
+    if (isPalindrome)
+        printf("The string is a palindrome.\n");
+    else
+        printf("The string is not a palindrome.\n");
+
+    return 0;
+}
